de330863ff489adbdfd742cc589b0e79e37ab694
Vrezh7 <vrejishxanyan57@gmail.com>
2022-07-08 16:34:56 +0400

    Homework

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+// let x = +prompt ("Number");
+// console.log(x*x)
+
+// let x = +prompt( "papa")
+// let z = +prompt("mama")
+// // console.log(x+z)
+
+
+// let x = +prompt( "chapahas")
+// if (x=>18){
+//     console.log("chapahasa")
+// }else{
+//     console.log("chapahas chi")
+
+
+
+let x = +prompt( "")
+if (x <= 7) {
+    alert('1 shapat');
+}
+else if(x <=14){
+    alert("2 shapat")
+}
+else if(x <=21 ){
+    alert("3 shapat")
+}
+else if(x<=28){
+    alert("4 shapat")
+}
+else if (x<=31){
+    alert("5 shapat")
+}
